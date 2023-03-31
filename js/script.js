09957d6cf20345ffa502363b36766dbe853d4e37
rodeomar <raed.alkhanbashi@gmail.com>
2023-03-31 13:26:29 -0700

    change application and write a HTML and Javascript

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-function neighborhood(clean) {
-  const cleanArray = clean.toString().split("");
-  const doubledCleanArray = [];
-  if (cleanArray.length <= 16 && cleanArray.length > 14) {
-    for (let i = 0; i < cleanArray.length; i++) {
-      if (i % 2 !== 0) {
-        let wordDouble = cleanArray[i] * 2
-        if (wordDouble > 9) {
-          const wordedDouble = wordDouble.toString().split("");
-          let word = 0;
-          wordedDouble.forEach(function(number) {
-            word += parseInt(number)
-          })
 
+function rioList() {
+  let input = document.getElementById("number").value;
+  let output = " ";
+  let list = [];
+  for (let i = 0; i <= input; i++) {
+    list.push(i);
+  }
+  output = list.join(",");
+  document.getElementById("result").innerHTML = output;
 
+  function rioList(num) {
+    let output = "";
+    for (let i = 0; i <= num; i++) {
+      let numStr = i.toString();
+      if (numStr.includes("3")) {
+        output += "a little bit more, ";
+      } else if (numStr.includes("2")) {
+        output += "keep counting, ";
+      } else if (numStr.includes("1")) {
+        output += "keep going, ";
+      } else {
+        output += i + ", ";
+      }
+    }
+    return output.slice(0,-2); // remove the trailing comma and space
+  }
+  
+  let num = parseInt(prompt("Enter a number:"));
+  let list1 = rioList(num);
 
+}
