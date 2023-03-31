98287b50f90d98b0e1839c8c8ce858fd73eeb1ba
rodeomar <raed.alkhanbashi@gmail.com>
2023-03-31 11:19:12 -0700

    add arrays

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Test: "It should return true if input is 0"
 Code: validate(0)
 Expected Output: true;
 
+Test: "It should return true if a number ends in 0"
+Code: validate(20);
+Expected Output: true;
+
+Test: "It should return false if a number does not end in zero"
+Code: validate(21);
+Expected Output: false;
--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ function neighborhood(clean) {
   const doubledCleanArray = [];
   if (cleanArray.length <= 16 && cleanArray.length > 14) {
     for (let i = 0; i < cleanArray.length; i++) {
+      if (i % 2 !== 0) {
+        let wordDouble = cleanArray[i] * 2
+        if (wordDouble > 9) {
+          const wordedDouble = wordDouble.toString().split("");
+          let word = 0;
+          wordedDouble.forEach(function(number) {
+            word += parseInt(number)
+          })
 
 
 
